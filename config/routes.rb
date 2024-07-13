46eb892226056cddf9f78fedc7925803872ae857
wassupdee <daichi.hirohata.japan@gmail.com>
2024-07-13 18:13:54 +0900

    質問の表示と、回答の保存を別のコントローラーに振り分けた

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
-  resources :questions, only: [:show, :create]
-  # get 'question1', to: 'q_and_a#question1'
-  # get 'question2', to: 'q_and_a#question2'
-  # get 'question3', to: 'q_and_a#question3'
-  post 'answer1', to: 'questions#answer1'
-  post 'answer2', to: 'questions#answer2'
-  post 'answer3', to: 'questions#answer3'
+  resources :questions, only: [:show]
+  resources :answers, only: [:create]
   get 'openai', to: 'openai_api#chat'
 end
