747fa07b2f76ce7d29a9ed7cf4a13c2c7b722408
wassupdee <daichi.hirohata.japan@gmail.com>
2024-07-14 16:02:29 +0900

    AIメッセージ提案の導入部分を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
+  resources :introductions, only: [:show]
   resources :questions, only: [:show]
   resources :answers, only: [:create]
   get 'openai', to: 'openai_api#chat'
