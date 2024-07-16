dc559ad89239cc4ef0cfa99cc6dac5e905748fd0
wassupdee <daichi.hirohata.japan@gmail.com>
2024-07-16 17:27:15 +0900

    ログアウト時のフラッシュメッセージが表示されるように修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -16,8 +16,8 @@ class UserSessionsController < ApplicationController
   end
 
   def destroy
-    flash[:success] = 'ログアウトしました'
     logout
+    flash[:success] = 'ログアウトしました'
     redirect_to root_path, status: :see_other
   end
 end
