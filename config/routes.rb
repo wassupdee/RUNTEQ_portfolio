37133d1102b4959f93eb8b0646387f8deadd9afd
wassupdee <daichi.hirohata.japan@gmail.com>
2024-07-16 15:38:36 +0900

    ログアウト機能のルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
   get 'openai', to: 'openai_api#chat'
 end
