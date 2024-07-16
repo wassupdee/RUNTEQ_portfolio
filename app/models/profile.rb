3f2da8578b929620ed086bf225231a7cd533109d
wassupdee <daichi.hirohata.japan@gmail.com>
2024-07-16 19:28:25 +0900

    構文エラーの修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_many :groups_profiles, dependent: :destroy
-  has_many :groups, through :groups_profiles
+  has_many :groups, through: :groups_profiles
   has_many :albums, dependent: :destroy
   has_one :note, dependent: :destroy
   has_many :events, dependent: :destroy
