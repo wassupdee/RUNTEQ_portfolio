03d3973145f1999b8e28f82b7069dcb6e44c211b
wassupdee <daichi.hirohata.japan@gmail.com>
2024-07-18 12:06:36 +0900

    連絡先登録フォームの作成

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,27 @@
+class ProfilesController < ApplicationController
+  def new
+    @profile = Profile.new
+    @profile.events.build(name: '誕生日')
+    @profile.events.build(name: '大切な日')
+  end
+
+  def create
+    binding.pry
+    @profile = Profile.new(profile_params)
+    # @events = @profile.events.build(events_params)
+
+    if @profile.save && @events.save
+      flash[:success] = '連絡先を登録しました'
+      redirect_to root_path # profile_path(@profile)
+    else
+      flash[:danger] = '連絡先を登録できませんでした'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :furigana, :phone, :email, :line_name, :birthplace, :address, events: [:name, :date])
+  end
+end
