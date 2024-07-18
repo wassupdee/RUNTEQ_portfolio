1dd282b179f5a1a31acf559a1e4efecd307148d5
wassupdee <daichi.hirohata.japan@gmail.com>
2024-07-18 15:33:21 +0900

    accepts_nested_attributes_forをprofileモデルに設定し、eventsと一括で保存できるようにした

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,13 +6,9 @@ class ProfilesController < ApplicationController
   end
 
   def create
-    binding.pry
-    @profile = current_user.profiles.new(profile_params.except(:events))
+    @profile = current_user.profiles.new(profile_params)
 
     if @profile.save
-      events_params.each do |event_param|
-        @profile.events.create(event_param)
-      end
       flash[:success] = '連絡先を登録しました'
       redirect_to root_path # profile_path(@profile)
     else
@@ -24,12 +20,6 @@ class ProfilesController < ApplicationController
   private
 
   def profile_params
-    params.require(:profile).permit(:name, :furigana, :phone, :email, :line_name, :birthplace, :address, events: [:name, :date])
-  end
-
-  def events_params
-    params.require(:profile).fetch(:events, {}).values.map do |event|
-      event.permit(:name, :date)
-    end
+    params.require(:profile).permit(:name, :furigana, :phone, :email, :line_name, :birthplace, :address, events_attributes: [:name, :date])
   end
 end
