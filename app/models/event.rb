f8d54addf31d73d498fa75271075975b2d15f4b5
wassupdee <daichi.hirohata.japan@gmail.com>
2024-07-21 17:00:28 +0900

    モデル内のselfを削除し、シンプルにした

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,15 +4,15 @@ class Event < ApplicationRecord
   enum notification_enabled: { off: false, on: true }
 
   def is_ready_to_notify?
-    self.date.present? && self.notification_timing.present? && self.notification_enabled?
+    date.present? && notification_timing.present? && notification_enabled?
   end
 
   def is_to_be_sent_today?
-    change_to_current_year - (change_utc_to_jst(DateTime.now)).to_date == self.notification_timing
+    change_to_current_year - (change_utc_to_jst(DateTime.now)).to_date == notification_timing
   end
 
   def change_to_current_year
-    Date.new(Date.today.year, self.date.mon, self.date.mday)
+    Date.new(Date.today.year, date.mon, date.mday)
   end
 
   def change_utc_to_jst(datetime)
