cd8191ca11a132fc180d9ab959f7f761e8274a24
wassupdee <daichi.hirohata.japan@gmail.com>
2024-07-23 16:12:10 +0900

    saveメソッドを削除し、updateメソッドを設定。profileレコードが保存されると、自動的にeventsレコードも作成されるので、現時点ではsaveメソッドは不要と判断

--- app/models/form/event_collection.rb
+++ app/models/form/event_collection.rb
@@ -14,9 +14,12 @@ class Form::EventCollection < Form::Base
     self.events= attributes.map { |_, v| Event.new(v) }
   end
 
-  def save
+  def update
     Event.transaction do
-      self.events.map(&:save!)
+      self.events.each do |event|
+        existing_event = Event.find(event.id)
+        existing_event.update!(event.attributes.except("created_at", "updated_at"))
+      end
     end
   end
 end
