87d10f08e9a5ffc6938bfaa19b73af736b8b1ef5
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-02 13:12:30 +0900

    チェックボックスは１つ以上選択するよう制限をかけた

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,20 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.querySelector("#ai_questions");
+  form.addEventListener("submit", function(event) {
+    // チェックボックスの選択状況を確認する
+    const checkboxes = form.querySelectorAll('input[type="checkbox"]');
+    if (checkboxes.length === 0) {
+      return;
+    }
+
+    const isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+    if (!isChecked) {
+      alert("少なくとも1つ選択してください。");
+      event.preventDefault(); // フォーム送信をキャンセル
+    }
+  });
+});
