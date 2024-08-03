c99e443ab2eecdd0a30cb9f3c450a62b1a39b997
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-03 14:01:04 +0900

    環境変数をfetchで取得することで、取得失敗時にエラーを発生させるようにした

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -38,8 +38,8 @@ class LineBotController < ApplicationController
 
   def client
     @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_secret = ENV.fetch("LINE_CHANNEL_SECRET")
+      config.channel_token = ENV.fetch("LINE_CHANNEL_TOKEN")
     }
   end
 end
