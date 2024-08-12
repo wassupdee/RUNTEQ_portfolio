b47794829d28e55aa79f19fcad0ca34585bbc0b8
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-12 20:03:09 +0900

    ログを出力するコードを削除

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -17,8 +17,6 @@ class OauthsController < ApplicationController
       begin
         @user = create_from(provider)
         # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
-        Rails.logger.debug "User created: #{@user.inspect}"
-
         reset_session # protect from session fixation attack
         auto_login(@user)
         redirect_to root_path, :notice => "Logged in from #{provider.titleize}!"
