337d4ba1e42b8d9125c21057d72fd20c16d19679
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-16 11:38:16 +0900

    groupモデル、groups_profileモデルをfactorybotで作成し、profileとgroups_profilesのアソシエーションチェックを実装

---
+++ spec/factories/groups_profiles.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :groups_profile do
+  end
+end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -39,5 +39,45 @@ RSpec.describe Profile, type: :model do
         expect(Event.where(id: [event1.id, event2.id])).to be_empty
       end
     end
+
+    describe "groups_profilesとのアソシエーション" do
+      it "groups_profilesと1対多の関係にある" do
+        group1 = create(:group, user: user)
+        group2 = create(:group, user: user)
+
+        groups_profiles1 = create(:groups_profile, profile: profile, group: group1)
+        groups_profiles2 = create(:groups_profile, profile: profile, group: group2)
+
+        expect(profile.groups_profiles).to include(groups_profiles1, groups_profiles2)
+      end
+
+      it '関連するgroups_profilesがあっても、profileを削除でき、groups_profilesも削除される' do
+        group1 = create(:group, user: user)
+        group2 = create(:group, user: user)
+
+        groups_profiles1 = create(:groups_profile, profile: profile, group: group1)
+        groups_profiles2 = create(:groups_profile, profile: profile, group: group2)
+
+        expect{ profile.destroy }.to change { Profile.count }.by(-1)
+        expect(GroupsProfile.where(id: [groups_profiles1.id, groups_profiles2.id])).to be_empty
+      end
+    end
+
+    # describe "groupsとのアソシエーション" do
+    #   it "groupsと1対多の関係にある" do
+    #     group1 = create(:group, profile: profile)
+    #     group2 = create(:group, profile: profile)
+
+    #     expect(profile.groups).to include(group1, group2)
+    #   end
+
+    #   it '関連するgroupsがあっても、profileを削除でき、groupsも削除される' do
+    #     group1 = create(:group, profile: profile)
+    #     group2 = create(:group, profile: profile)
+
+    #     expect{ profile.destroy }.to change { Profile.count }.by(-1)
+    #     expect(group.where(id: [group1.id, group2.id])).to be_empty
+    #   end
+    # end
   end
 end
