bbe0782dd95d8af09038af32547ef65fdb169d9c
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-16 17:03:10 +0900

    eventモデルとprofileのアソシエーションテストを実装

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  let(:user) { create(:user) }
+  let(:profile) { create(:profile, user: user) }
+  let(:event) { create(:event, profile: profile) }
+
+  describe "アソシエーションチェック" do
+    describe "profileとのアソシエーション" do
+      it "profileと1対多の関係にある" do
+        expect(event.profile).to eq(profile)
+      end
+    end
+  end
+end
