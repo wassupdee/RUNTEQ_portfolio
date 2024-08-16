0039f111d1fdc71e32cc876e9efd44945225adfe
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-16 16:01:15 +0900

    answerモデルのユニークバリエーションのテストを実装

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,11 +6,19 @@ RSpec.describe Answer, type: :model do
   let(:answer) { create(:answer, question: question) }
 
   describe "アソシエーションチェック" do
-
     describe "questionとのアソシエーション" do
       it "questionと1対多の関係にある" do
         expect(answer.question).to eq(question)
       end
+    end
+  end
+
+  describe "バリデーションチェック" do
+    it "valueがユニークであること" do
+      answer = create(:answer, question: question, value: "unique_value")
+      duplicated_answer = build(:answer, question: question, value: "unique_value")
+
+      expect(duplicated_answer).to be_invalid
     end
   end
 end
