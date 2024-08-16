3a78692d4fa14958b65079dfc225d1211477241d
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-16 14:45:32 +0900

    groupモデルのprofilesとのアソシエーションテストを実装

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -10,5 +10,32 @@ RSpec.describe Group, type: :model do
         expect(group.user).to eq(user)
       end
     end
+
+    # describe "groups_profilesとのアソシエーション" do
+    #   let!(:group1) { create(:group, user: user) }
+    #   let!(:group2) { create(:group, user: user) }
+    #   let!(:groups_profiles1) { create(:groups_profile, profile: profile, group: group1) }
+    #   let!(:groups_profiles2) { create(:groups_profile, profile: profile, group: group2) }
+
+    #   it "groups_profilesと1対多の関係にある" do
+    #     expect(profile.groups_profiles).to include(groups_profiles1, groups_profiles2)
+    #   end
+
+    #   it '関連するgroups_profilesがあっても、profileを削除でき、groups_profilesも削除される' do
+    #     expect{ profile.destroy }.to change { Profile.count }.by(-1)
+    #     expect(GroupsProfile.where(id: [groups_profiles1.id, groups_profiles2.id])).to be_empty
+    #   end
+    # end
+
+    describe "profilesとのアソシエーション" do
+      let!(:profile1) { create(:profile, user: user) }
+      let!(:profile2) { create(:profile, user: user) }
+      let!(:groups_profiles1) { create(:groups_profile, profile: profile1, group: group) }
+      let!(:groups_profiles2) { create(:groups_profile, profile: profile2, group: group) }
+
+      it "groupsと1対多の関係にある" do
+        expect(group.profiles).to include(profile1, profile2)
+      end
+    end
   end
 end
