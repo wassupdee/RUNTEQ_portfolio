5c4903fbb2de46aa3d0a9d0e3b7171d2b3e583ca
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-16 17:39:38 +0900

    日付をUTCからJSTへ変換するロジックテストを実装

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -17,5 +17,24 @@ RSpec.describe Event, type: :model do
     it "通知の条件を満たしているか確認する" do
       expect(event.ready_to_notify?).to eq(true)
     end
+
+    it "日付をUTCからJSTへ変換する" do
+      utc = DateTime.new(2024,8,15,23,0,0)
+      event = create(:event, profile: profile, date: utc)
+      jst = event.change_utc_to_jst(utc)
+
+      expect(jst).to eq(DateTime.new(2024,8,16,8,0,0))
+    end
+
+    # it "通知日は今日か確認する" do
+    #   event = create(
+    #     :event,
+    #     profile: profile,
+    #     notification_timing: 1,
+    #     date: Date.tomorrow
+    #   )
+
+    #   expect(event.scheduled_to_notify_today?).to eq(true)
+    # end
   end
 end
