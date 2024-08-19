6e7c61d1e034d7cc3b30f17487bfb114074cfd55
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-19 19:21:18 +0900

    chromeのバージョンを指定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,6 @@
+require 'webdrivers'
+Webdrivers::Chromedriver.required_version = '114.0.5735.90'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
