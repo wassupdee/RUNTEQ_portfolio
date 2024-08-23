23d808f6cced2ac12a6c16ade66a029454f8f16a
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-23 12:49:11 +0900

    albumのCRUD操作用ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
         patch :update_all
       end
     end
+    resources :albums
   end
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
