6e2d750edc58549f73575e60c82d1497085f346f
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-23 17:47:28 +0900

    マイナスの演算子の後にスペースを追加

--- spec/factories/album.rb
+++ spec/factories/album.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :album do
-    sequence(:date) { |n| Date.today -n.days }
+    sequence(:date) { |n| Date.today - n.days }
     sequence(:title) { |n| "album_#{n}" }
     sequence(:diary) { |n| "diary_#{n}" }
   end
--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :event do
     sequence(:name) { |n| "event_#{n}" }
-    sequence(:date) { |n| Date.today -n.days }
+    sequence(:date) { |n| Date.today - n.days }
     sequence(:notification_timing) { |n| n }
     notification_enabled { true }
   end
