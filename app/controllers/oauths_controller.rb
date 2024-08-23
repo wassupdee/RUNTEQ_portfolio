2b2d7d8dc9bb4e7a6e456cdb374bce74c98ed0ef
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-23 18:48:21 +0900

    rescueメソッドに対象のエラークラスを指定

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -20,7 +20,7 @@ class OauthsController < ApplicationController
         reset_session # protect from session fixation attack
         auto_login(@user)
         redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
-      rescue
+      rescue StandardError => e
         redirect_to root_path, alert: "Failed to login from #{provider.titleize}!"
       end
     end
