e731f90c116d835ad91f83aff3db3ef203697aaf
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-26 18:02:10 +0900

    記述ミスを修正

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -10,8 +10,7 @@ class OauthsController < ApplicationController
 
   def callback
     provider = auth_params[:provider]
-
-    if @user == login_from(provider)
+    if @user = login_from(provider)
       redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
     else
       new_user_login(provider)
