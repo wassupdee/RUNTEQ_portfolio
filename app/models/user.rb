ac16f94f9f6be6857b6a6f6de862711ac4a47d8a
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-27 18:17:53 +0900

    userのnameカラムに、空欄を許容しないバリデーションを再設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  # validates :name, presence: true, length: { maximum: 255 }
+  validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
   validates :line_user_id, uniqueness: true, allow_blank: true
   validates :notification_enabled, presence: true
