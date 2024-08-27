ec26d4a9ea41e72820b5e8562467657a7061191f
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-27 13:23:39 +0900

    パスワードリセット用のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :questions, only: [:show]
   resources :answers, only: [:create]
   resources :users, only: [:new, :create]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   resources :profiles do
     resources :events do
       collection do
