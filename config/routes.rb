6d3840fe1cd5ba684c874d65b5f3ab82345d960a
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-27 13:35:01 +0900

    開発環境では、letter opener webエンジンをアプリにマウントし、エンジンにアクセスするURLを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   root 'static_pages#top'
   resources :introductions, only: [:show]
   resources :questions, only: [:show]
