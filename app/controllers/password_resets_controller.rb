d8369e7aa15c5d1e7756da96f6a55b36ac382417
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-27 17:05:21 +0900

    パスワードリセットのコントローラーの中の古い記述を変更

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,36 +1,34 @@
 class PasswordResetsController < ApplicationController
   skip_before_action :require_login
 
+  def new; end
+
   def create
-    @user = User.find_by_email(params[:email])
-    @user.deliver_reset_password_instructions! if @user
-    redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
+    @user = User.find_by(email: params[:email])
+    @user&.deliver_reset_password_instructions!
+    flash[:success] = "パスワードリセットのメールを送信しました"
+    redirect_to login_path
   end
 
   def edit
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
-
-    if @user.blank?
-      not_authenticated
-      return
-    end
+    not_authenticated if @user.blank?
   end
 
   def update
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
 
-    if @user.blank?
-      not_authenticated
-      return
-    end
+    not_authenticated if @user.blank?
 
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
-      redirect_to(root_path, :notice => 'Password was successfully updated.')
+      flash[:success] = "パスワードがリセットされました"
+      redirect_to login_path
     else
-      render :action => "edit"
+      flash.now[:danger] = "パスワードリセットに失敗しました"
+      render :edit, status: :unprocessable_entity
     end
   end
 end
