e84f83cd3797aa35b1f66919608a225e9b17f4fe
wassupdee <daichi.hirohata.japan@gmail.com>
2024-08-28 11:18:57 +0900

    アルバム画像imagesにバリデーションを設定

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,9 @@ class Album < ApplicationRecord
   belongs_to :profile
 
   has_many_attached :images
+
+  validates :images, processable_image: true,
+                     content_type: %i[png jpeg],
+                     size: { less_than: 5.megabytes , message: 'is too large' },
+                     limit: { max: 3 }
 end
