467511a76f531de4de1bfe7232d3a98afc944197
wassupdee <daichi.hirohata.japan@gmail.com>
2024-09-01 16:43:48 +0900

    不要なスペースを削除

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,7 +5,7 @@ class ImagesController < ApplicationController
 
     render turbo_stream: [
       turbo_stream.remove("album_#{@image.record_id}-#{@image.id}"),
-      turbo_stream.replace("album_hidden_fields", partial: "albums/hidden_fields", locals: { album: @image.record } )
+      turbo_stream.replace("album_hidden_fields", partial: "albums/hidden_fields", locals: { album: @image.record })
     ]
   end
 end
