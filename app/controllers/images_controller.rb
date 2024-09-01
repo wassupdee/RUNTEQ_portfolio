370448bd843ee26fb93776c3e54e995f887dfd5e
wassupdee <daichi.hirohata.japan@gmail.com>
2024-09-01 15:21:58 +0900

    画像が非同期で削除された場合、hidden fieldを置換し、削除された画像がパラメーターに入らないようにする

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,5 +2,10 @@ class ImagesController < ApplicationController
   def destroy
     @image = ActiveStorage::Attachment.find(params[:id])
     @image.purge
+
+    render turbo_stream: [
+      turbo_stream.remove("album_#{@image.record_id}-#{@image.id}"),
+      turbo_stream.replace("album_hidden_fields", partial: "albums/hidden_fields", locals: { album: @image.record } )
+    ]
   end
 end
