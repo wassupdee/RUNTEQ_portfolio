c27c5ee61fb317046b631c5f9468f2656a48885e
wassupdee <daichi.hirohata.japan@gmail.com>
2024-09-21 14:30:10 +0900

    factorybotのeventを修正（notification_timingの数値の選択肢を決め、ランダムで選ぶ）

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :event do
     sequence(:name) { |n| "event_#{n}" }
     sequence(:date) { |n| Date.today - n.days }
-    sequence(:notification_timing) { |n| n }
+    notification_timing { [0,1,3,7,14,30,60].sample }
     notification_enabled { true }
   end
 end
