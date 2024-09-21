f1f6e1a29ed51a1c96d1fcd98ef5cf6f86c8dd7e
wassupdee <daichi.hirohata.japan@gmail.com>
2024-09-21 14:16:33 +0900

    factorybotのprofileを修正（last_contacted, note追加。birthplace, occupation削除)

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     sequence(:phone) { |n| "0901234#{format('%04d', n)}" }
     sequence(:email) { |n| "test_#{n}@example.com" }
     sequence(:line_name) { |n| "profile_#{n}" }
-    sequence(:birthplace) { |n| "birthplace_#{n}" }
     sequence(:address) { |n| "address_#{n}" }
-    sequence(:occupation) { |n| "occupation_#{n}" }
+    sequence(:note) { |n| "note_#{n}" }
+    last_contacted { %w[within_one_month within_one_year within_three_years more_than_three_years].sample }
   end
 end
