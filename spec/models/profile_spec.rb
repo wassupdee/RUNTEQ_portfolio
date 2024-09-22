4dc45c9574884d20cc9cff0faae51da6901e7717
wassupdee <daichi.hirohata.japan@gmail.com>
2024-09-22 14:06:29 +0900

    group, groups_profilesとのアソシエーションテストをコメントアウト（groupとgroups_profilesはアプリで未使用のため）

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -39,32 +39,32 @@ RSpec.describe Profile, type: :model do
       end
     end
 
-    describe "groups_profilesとのアソシエーション" do
-      let!(:group1) { create(:group, user:) }
-      let!(:group2) { create(:group, user:) }
-      let!(:groups_profiles1) { create(:groups_profile, profile:, group: group1) }
-      let!(:groups_profiles2) { create(:groups_profile, profile:, group: group2) }
+    # describe "groups_profilesとのアソシエーション" do
+    #   let!(:group1) { create(:group, user:) }
+    #   let!(:group2) { create(:group, user:) }
+    #   let!(:groups_profiles1) { create(:groups_profile, profile:, group: group1) }
+    #   let!(:groups_profiles2) { create(:groups_profile, profile:, group: group2) }
 
-      it "groups_profilesと1対多の関係にある" do
-        expect(profile.groups_profiles).to include(groups_profiles1, groups_profiles2)
-      end
+    #   it "groups_profilesと1対多の関係にある" do
+    #     expect(profile.groups_profiles).to include(groups_profiles1, groups_profiles2)
+    #   end
 
-      it "関連するgroups_profilesがあっても、profileを削除でき、groups_profilesも削除される" do
-        expect { profile.destroy }.to change { Profile.count }.by(-1)
-        expect(GroupsProfile.where(id: [groups_profiles1.id, groups_profiles2.id])).to be_empty
-      end
-    end
+    #   it "関連するgroups_profilesがあっても、profileを削除でき、groups_profilesも削除される" do
+    #     expect { profile.destroy }.to change { Profile.count }.by(-1)
+    #     expect(GroupsProfile.where(id: [groups_profiles1.id, groups_profiles2.id])).to be_empty
+    #   end
+    # end
 
-    describe "groupsとのアソシエーション" do
-      let!(:group1) { create(:group, user:) }
-      let!(:group2) { create(:group, user:) }
-      let!(:groups_profiles1) { create(:groups_profile, profile:, group: group1) }
-      let!(:groups_profiles2) { create(:groups_profile, profile:, group: group2) }
+    # describe "groupsとのアソシエーション" do
+    #   let!(:group1) { create(:group, user:) }
+    #   let!(:group2) { create(:group, user:) }
+    #   let!(:groups_profiles1) { create(:groups_profile, profile:, group: group1) }
+    #   let!(:groups_profiles2) { create(:groups_profile, profile:, group: group2) }
 
-      it "groupsと1対多の関係にある" do
-        expect(profile.groups).to include(group1, group2)
-      end
-    end
+    #   it "groupsと1対多の関係にある" do
+    #     expect(profile.groups).to include(group1, group2)
+    #   end
+    # end
   end
 
   describe "accepts_nested_attributes_for" do
