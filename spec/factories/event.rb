5079512bb593e8946704c3c5c2df85e86c2d9c14
wassupdee <daichi.hirohata.japan@gmail.com>
2024-09-22 15:07:05 +0900

    配列の要素とコンマの間にスペースを挿入

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :event do
     sequence(:name) { |n| "event_#{n}" }
     sequence(:date) { |n| Date.today - n.days }
-    notification_timing { [0,1,3,7,14,30,60].sample }
+    notification_timing { [0, 1, 3, 7, 14, 30, 60].sample }
     notification_enabled { true }
   end
 end
