47670eae0c64e78c4f3cf1427b91b2973c80455f
wassupdee <daichi.hirohata.japan@gmail.com>
2024-09-25 15:14:55 +0900

    カレンダーに連絡先の名前とイベント名を表示

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,6 @@
 class CalendarsController < ApplicationController
   def index
-    # profiles = current_user.profiles
-    # @events = profiles.flat_map(&:events)
+    profiles = current_user.profiles
+    @events = profiles.flat_map(&:events)
   end
 end
