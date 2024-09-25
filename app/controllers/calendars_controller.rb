df500e63d011ef4bfb6c1c6029d99c155546cfc5
wassupdee <daichi.hirohata.japan@gmail.com>
2024-09-25 16:06:30 +0900

    誕生日・大切な日がどの年のカレンダーにも表示されるようにした

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,12 @@
 class CalendarsController < ApplicationController
   def index
     profiles = current_user.profiles
-    @events = profiles.flat_map(&:events)
+    events = profiles.flat_map(&:events)
+    year = params[:start_date].to_date.year
+
+    @events_this_year = events.map do |event|
+      event.date = event.date.change(year:)
+      event
+    end
   end
 end
