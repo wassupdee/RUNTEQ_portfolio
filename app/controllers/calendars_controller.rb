38eefc395ec1a741baf0a63a958c32bfbaad14dd
wassupdee <daichi.hirohata.japan@gmail.com>
2024-09-25 18:28:22 +0900

    　各eventのyearを変更する@yearを、パラメーターが無い場合は、今日の日付から年を取得し代入するロジックを実装

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,11 +1,17 @@
 class CalendarsController < ApplicationController
+  before_action :set_year, only: %i[index]
+
   def index
     events = current_user.profiles.flat_map(&:events)
-    year = params[:start_date].to_date.year
-
     @events_this_year = events.map do |event|
-      event.date = event.date.change(year:)
+      event.date = event.date.change(year: @year)
       event
     end
+  end
+
+  private
+
+  def set_year
+    @year = params[:start_date].present? ? params[:start_date].to_date.year : Date.today.year
   end
 end
