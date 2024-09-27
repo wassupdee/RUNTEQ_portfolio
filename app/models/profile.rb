c77ab477ec00e4a6079109c55a6fd0eb616b2a78
wassupdee <daichi.hirohata.japan@gmail.com>
2024-09-27 12:27:21 +0900

    groupとprofileモデルに、一対多の関係を記述

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  belongs_to :group
   has_many :groups_profiles, dependent: :destroy
   has_many :groups, through: :groups_profiles
   has_many :albums, dependent: :destroy
