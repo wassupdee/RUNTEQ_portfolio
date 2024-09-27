b80e35c733d89dd5661bfb7eb6cdc4443b1d539e
wassupdee <daichi.hirohata.japan@gmail.com>
2024-09-27 15:25:58 +0900

    リファクタリングを実施

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,7 @@
 class ProfilesController < ApplicationController
   before_action :set_q, only: %i[index]
+  before_action :set_profile_record, only: %i[edit update destroy]
+
   def new
     @profile = Profile.new
     @profile.events.build(name: "誕生日")
@@ -20,21 +22,12 @@ class ProfilesController < ApplicationController
   def create
     @profile = current_user.profiles.new(profile_params.except(:group))
     @profile.group = Group.find(profile_params[:group][:id])
-    if @profile.save
-      flash[:success] = "連絡先を登録しました"
-      redirect_to profile_path(@profile)
-    else
-      flash[:danger] = "連絡先を登録できませんでした"
-      render :new, status: :unprocessable_entity
-    end
+    profile_save
   end
 
-  def edit
-    @profile = current_user.profiles.includes(:events).find(params[:id])
-  end
+  def edit; end
 
   def update
-    @profile = current_user.profiles.find(params[:id])
     @profile.group = Group.find(profile_params[:group][:id])
     if @profile.update(profile_params.except(:group))
       update_success
@@ -45,7 +38,6 @@ class ProfilesController < ApplicationController
   end
 
   def destroy
-    @profile = current_user.profiles.find(params[:id])
     @profile.destroy!
 
     respond_to do |format|
@@ -74,5 +66,19 @@ class ProfilesController < ApplicationController
     @q = current_user.profiles.ransack(params[:q])
     @q.combinator = "or"
     @q.sorts = "name asc" if @q.sorts.empty?
+  end
+
+  def set_profile_record
+    @profile = current_user.profiles.find(params[:id])
+  end
+
+  def profile_save
+    if @profile.save
+      flash[:success] = "連絡先を登録しました"
+      redirect_to profile_path(@profile)
+    else
+      flash[:danger] = "連絡先を登録できませんでした"
+      render :new, status: :unprocessable_entity
+    end
   end
 end
