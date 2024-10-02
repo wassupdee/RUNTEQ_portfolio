4b434a05c482d4c5cbf875c17ccd3957a89d8e1c
wassupdee <daichi.hirohata.japan@gmail.com>
2024-10-02 16:59:10 +0900

    albumのバリエーションテストを修正（上限枚数を３→２枚、サイズを５→３MBに変更）

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -22,19 +22,19 @@ RSpec.describe Album, type: :model do
         album.images.attach(invalide_content_type)
         expect(album).to be_invalid
       end
-      it "５MB以上のファイルはアップロードできない" do
+      it "3MB以上のファイルはアップロードできない" do
         album.images.attach(large_image)
         expect(album).to be_invalid
       end
-      it "画像は４枚以上アップロードできない" do
-        4.times { album.images.attach(valid_image) }
+      it "画像は3枚以上アップロードできない" do
+        3.times { album.images.attach(valid_image) }
         expect(album).to be_invalid
       end
     end
 
     context "有効なデータの場合" do
       it "正常に画像が保存される" do
-        3.times { album.images.attach(valid_image) }
+        2.times { album.images.attach(valid_image) }
         expect(album).to be_valid
       end
     end
