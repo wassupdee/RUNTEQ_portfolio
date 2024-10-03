1fefbb79c905c123d6e890e0f7ae76eb571a5bc6
wassupdee <daichi.hirohata.japan@gmail.com>
2024-10-03 12:56:24 +0900

    specテストの簡単なものだけ実装（他は一旦コメントアウトし、GitHubActionsの動作確認をする）

--- spec/system/top_page_spec.rb
+++ spec/system/top_page_spec.rb
@@ -1,21 +1,28 @@
 require "rails_helper"
 
 RSpec.describe "top_page", type: :system do
-  before do
+  # before do
+  #   visit root_path
+  # end
+
+  it "メインメッセージが表示されること" do
     visit root_path
+    expect(page).to have_content "また、つながろう。"
   end
 
   describe "ページ遷移確認" do
     context "AIメッセージ作成ボタンをクリック" do
       it "AIメッセージページに遷移する" do
+        visit root_path
         click_link "メッセージを作成する"
         expect(page).to have_current_path(introduction_path(1))
       end
     end
     context "連絡帳ボタンをクリック" do
       it "連絡帳ページに遷移する" do
+        visit root_path
         click_link "ともだち帳をひらく"
-        expect(page).to have_current_path(profiles_path)
+        expect(page).to have_current_path(login_path)
       end
     end
   end
