1728d246f775dd84a2fe14feab35019c2b71bc4f
wassupdee <daichi.hirohata.japan@gmail.com>
2024-10-04 11:32:13 +0900

    パンくずリストの間隔を広げ、リンクには下線を引き、現在のページはフォントを大きくした

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,25 +1,42 @@
 crumb :root do
-  link "Top", root_path
+  link "<span class='link'>Top</span>".html_safe, root_path
 end
 
 crumb :question_1 do |question|
-  link "質問#{question.number}", question_path(question.number)
+  current_question_id = params[:id].to_i
+  if question.number == current_question_id
+    link "<span class='font-bold text-lg'>質問#{question.number}</span>".html_safe, question_path(question.number)
+  else
+    link "<span class='link'>質問#{question.number}</span>".html_safe, question_path(question.number)
+  end
   parent :root
 end
 
 crumb :question_2 do |question|
   parent_question = Question.find_by(number: question.number - 1)
-  link "質問#{question.number}", question_path(question.number)
+
+  current_question_id = params[:id].to_i
+  if question.number == current_question_id
+    link "<span class='font-bold text-lg'>質問#{question.number}</span>".html_safe, question_path(question.number)
+  else
+    link "<span class='link'>質問#{question.number}</span>".html_safe, question_path(question.number)
+  end
   parent :question_1, parent_question
 end
 
 crumb :question_3 do |question|
   parent_question = Question.find_by(number: question.number - 1)
-  link "質問#{question.number}", question_path(question.number)
+
+  current_question_id = params[:id].to_i
+  if question.number == current_question_id
+    link "<span class='font-bold text-lg'>質問#{question.number}</span>".html_safe, question_path(question.number)
+  else
+    link "<span class='link'>質問#{question.number}</span>".html_safe, question_path(question.number)
+  end
   parent :question_2, parent_question
 end
 
 crumb :ai_message do |question|
-  link "AI提案文", ai_message_path
+  link "<span class='font-bold text-lg'>AI提案文</span>".html_safe, ai_message_path
   parent :question_3, question
 end
