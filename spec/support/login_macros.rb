8bf5d7fadc4cea237f44bfab8bc1ddf07861dbb4
wassupdee <daichi.hirohata.japan@gmail.com>
2024-10-18 16:47:39 +0900

    loginマクロのログインリンクは、最初にマッチするものをクリックするように指定

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,7 +1,7 @@
 module LoginMacros
   def login_as(user)
     visit root_path
-    click_link "ログイン"
+    click_link "ログイン", match: :first
     fill_in "メールアドレス", with: user.email
     fill_in "パスワード", with: "password"
     click_button "ログイン"
