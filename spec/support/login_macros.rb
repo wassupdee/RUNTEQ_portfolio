3bfd3e27cb76356922c7c50a3aeae8d7a2b8a5ae
wassupdee <daichi.hirohata.japan@gmail.com>
2024-10-18 18:31:55 +0900

    loginモジュールで、ログインが完了するまで待つコードを追加

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,9 +1,10 @@
 module LoginMacros
   def login_as(user)
-    visit root_path
-    click_link "ログイン", match: :first
+    visit login_path
     fill_in "メールアドレス", with: user.email
     fill_in "パスワード", with: "password"
     click_button "ログイン"
+    # ログイン処理が完了することを担保する
+    expect(page).to have_current_path(root_path)
   end
 end
