278248cd5aaf1bf3bfaaa851fc7671bfc4d3df52
wassupdee <daichi.hirohata.japan@gmail.com>
2024-10-18 19:14:41 +0900

    未ログインの場合、連絡帳ボタンをクリックすると、ログインページに遷移する

--- spec/system/top_page_spec.rb
+++ spec/system/top_page_spec.rb
@@ -29,14 +29,14 @@ RSpec.describe "top_page", type: :system do
       end
     end
 
-  #   describe "ページ遷移確認" do
-  #     context "AIメッセージ作成ボタンをクリック" do
-  #       it "AIメッセージページに遷移する" do
-  #         visit root_path
-  #         click_link "メッセージを作成する"
-  #         expect(page).to have_current_path(introduction_path(1))
-  #       end
-  #     end
+    context "連絡帳ボタンをクリック" do
+      it "ログインページに遷移する" do
+        visit root_path
+        click_link "ともだち帳をひらく"
+        expect(page).to have_current_path(login_path)
+      end
+    end
+
   #     context "連絡帳ボタンをクリック" do
   #       it "連絡帳ページに遷移する" do
   #         visit root_path
