984763d643ee5104da3107c7a084465501d8bbc2
wassupdee <daichi.hirohata.japan@gmail.com>
2024-10-18 17:42:04 +0900

    確認用パスワードが空欄の場合、ユーザーの新規作成が失敗するテストを実装

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -87,6 +87,19 @@ RSpec.describe "UserSessions", type: :system do
           expect(page).to have_current_path(new_user_path)
         end
       end
+
+      context "確認用パスワードが空欄" do
+        it "ユーザーの新規作成が失敗する" do
+          visit new_user_path
+          fill_in "名前", with: "テスト"
+          fill_in "メールアドレス", with: "email@example.com"
+          fill_in "パスワード", with: "password"
+          fill_in "パスワード再入力", with: ""
+          click_button "登録"
+          expect(page).to have_content "ユーザー登録に失敗しました"
+          expect(page).to have_current_path(new_user_path)
+        end
+      end
     end
   end
 end
