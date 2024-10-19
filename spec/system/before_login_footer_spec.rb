6e1e6e52ae19a7fc2598614a660f500d50ef5929
wassupdee <daichi.hirohata.japan@gmail.com>
2024-10-19 14:31:25 +0900

    PC画面でのログイン前フッターのリンク遷移テストを実装

---
+++ spec/system/before_login_footer_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe "before_login_header", type: :system do
+  let(:user) { create(:user) }
+  before { visit root_path }
+
+  describe "PC画面" do
+    describe "ページ遷移確認" do
+      context "利用規約ボタンをクリック" do
+        it "利用規約ページに遷移する" do
+          within("#pc-footer") do
+            click_link "利用規約"
+          end
+          expect(page).to have_current_path(terms_path)
+        end
+      end
+
+      context "プライバシーポリシーボタンをクリック" do
+        it "プライバシーポリシーページに遷移する" do
+          within("#pc-footer") do
+            click_link "プライバシーポリシー"
+          end
+          expect(page).to have_current_path(privacy_policy_path)
+        end
+      end
+
+      context "お問い合わせボタンをクリック" do
+        it "お問い合わせページに遷移する" do
+          within("#pc-footer") do
+            click_link "お問い合わせ"
+          end
+          expect(page).to have_current_path("https://docs.google.com/forms/d/e/1FAIpQLScyntUUOKze4OmcuIShk5VLcZu-8JEHIAqB6GCKeti32fG6vg/viewform")
+        end
+      end
+    end
+  end
+end
