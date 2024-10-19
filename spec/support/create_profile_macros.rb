2468ae9469357fcc0bf0d107e586cf6ebf470a29
wassupdee <daichi.hirohata.japan@gmail.com>
2024-10-19 20:29:21 +0900

    profileを2レコード作成するよう実装

--- spec/support/create_profile_macros.rb
+++ spec/support/create_profile_macros.rb
@@ -1,7 +1,7 @@
 module CreateProfileMacros
-  def create_profile
-    page.driver.browser.manage.window.resize_to(1280, 900)
+  def create_profile_1
     visit profiles_path
+    page.driver.browser.manage.window.resize_to(1280, 900)
     within("#pc-profiles") do
       click_button "連絡先を作成"
     end
@@ -16,6 +16,27 @@ module CreateProfileMacros
     fill_in "LINEの名前", with: "やまだ"
     select "１年前", from: "最後に連絡した日"
     fill_in "メモ", with: "メモテスト"
+    attach_file "プロフィール画像", Rails.root.join("spec/fixtures/files/valid_image.jpg")
+    click_button "登録する"
+    expect(page).to have_content "連絡先を登録しました"
+    expect(page).to have_current_path(profile_path(Profile.last))
+  end
+
+  def create_profile_2
+    visit profiles_path
+    page.driver.browser.manage.window.resize_to(1280, 900)
+    within("#pc-profiles") do
+      click_button "連絡先を作成"
+    end
+    fill_in "名前", with: "佐藤一郎"
+    fill_in "フリガナ", with: "サトウイチロウ"
+    fill_in "誕生日", with: "01-15-1990"
+    fill_in "大切な日", with: "入社記念日"
+    find_all("input[type='date']")[1].set("5-10-2015")
+    fill_in "電話番号", with: "090-1234-5678"
+    fill_in "メールアドレス", with: "ichiro.sato@example.com"
+    fill_in "住所", with: "大阪府大阪市北区梅田1-1-1"
+    fill_in "LINEの名前", with: "いちろう"
     attach_file "プロフィール画像", Rails.root.join("spec/fixtures/files/valid_image.jpg")
     click_button "登録する"
     expect(page).to have_content "連絡先を登録しました"
