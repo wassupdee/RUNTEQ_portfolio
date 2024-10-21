90dce9be11b83196535d8bd4faa1f9a6d83c3fff
wassupdee <daichi.hirohata.japan@gmail.com>
2024-10-21 16:57:20 +0900

    未回答の場合、エラーが起きるテストを実装

--- spec/system/ai_messages_spec.rb
+++ spec/system/ai_messages_spec.rb
@@ -7,11 +7,10 @@ RSpec.describe "ai_messages", type: :system do
 
   context "回答が正常の場合" do
     it "メッセージを提案すること" do
-
       # OpenAI Clientのモックをつくる
       openai_client_mock = double("OpenAI client")
 
-      #OpenAI Clientからchatメソッドが呼び出され、レスポンスを設定。レスポンスからdigメソッドが呼び出されるのでハッシュにする
+      # OpenAI Clientからchatメソッドが呼び出され、レスポンスを設定。レスポンスからdigメソッドが呼び出されるのでハッシュにする
       allow(openai_client_mock).to receive(:chat).and_return(
         {
           "choices" => [
@@ -44,6 +43,40 @@ RSpec.describe "ai_messages", type: :system do
 
       # 本物のサービスインスタンスからメソッドを呼び出す。ただし、実際に呼び出されるのはmock
       expect(ai_suggest_service.suggest).to eq("AIが提案したメッセージです！")
+    end
+  end
+
+  context "質問1の回答が未選択の場合" do
+    it "質問2に遷移できない" do
+      visit introduction_path(1)
+      click_button "質問にすすむ"
+      click_button "次へ"
+      expect(page).to have_current_path(question_path(1))
+    end
+  end
+
+  context "質問2の回答が未選択の場合" do
+    it "質問3に遷移できない" do
+      visit introduction_path(1)
+      click_button "質問にすすむ"
+      choose "answer[question1]_friend"
+      click_button "次へ"
+      click_button "次へ"
+      alert = page.driver.browser.switch_to.alert
+      expect(alert.text).to eq("少なくとも1つ選択してください。")
+    end
+  end
+
+  context "質問3の回答が未選択の場合" do
+    it "AIメッセージ提案ページに遷移できない" do
+      visit introduction_path(1)
+      click_button "質問にすすむ"
+      choose "answer[question1]_friend"
+      click_button "次へ"
+      check "answer[question2]_life"
+      click_button "次へ"
+      click_button "次へ"
+      expect(page).to have_current_path(question_path(3))
     end
   end
 end
