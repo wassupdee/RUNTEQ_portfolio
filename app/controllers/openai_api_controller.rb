93ef727300e04dd65e49019bc217ab5e1171d878
wassupdee <daichi.hirohata.japan@gmail.com>
2024-10-21 16:37:49 +0900

    OpenAI API clientインスタンスの作成を、サービス側のプライベートメソッドで定義した（mockテスト用にサービスとクライアントの作成を分離）

--- app/controllers/openai_api_controller.rb
+++ app/controllers/openai_api_controller.rb
@@ -1,18 +1,9 @@
 class OpenaiApiController < ApplicationController
   skip_before_action :require_login
-  before_action :set_client
 
   def show
-    ai_suggest_service = AiSuggestionService.new(@client, session)
-
+    ai_suggest_service = AiSuggestionService.new(session)
     @message = ai_suggest_service.suggest
     @last_question = Question.order(number: :desc).first
-  end
-
-  private
-
-  # config/initializerにAPI Keyを設定しているので、新しいインスタンスをつくるだけでKeyを読み込んでくれる
-  def set_client
-    @client = OpenAI::Client.new
   end
 end
